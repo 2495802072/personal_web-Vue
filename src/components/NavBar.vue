23c91beba371994a7c851eb5ad97f8b34f431a5f
顾霖轩 <2495802072@qq.com>
2024-10-18 10:32:47 +0800

    部署修缮

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -10,23 +10,23 @@ export default defineComponent({
         theme_dark: true,
         nav_list: [{ // pages 加完记得修改router/index.ts
           link_to: '/',
-          icon_url: '/src/assets/svg_icon/home-alt.svg',
+          icon_url: 'src/assets/svg_icon/home-alt.svg',
           text: '首页'
         },{
           link_to: '/archives',
-          icon_url: '/src/assets/svg_icon/id-badge-2.svg',
+          icon_url: 'src/assets/svg_icon/id-badge-2.svg',
           text: '档案'
         },{
           link_to: '/categories',
-          icon_url: '/src/assets/svg_icon/grid.svg',
+          icon_url: 'src/assets/svg_icon/grid.svg',
           text: '分类'
         },{
           link_to: '/messages',
-          icon_url: '/src/assets/svg_icon/envelope-alt.svg',
+          icon_url: 'src/assets/svg_icon/envelope-alt.svg',
           text: '消息'
         },{
           link_to: '/projects',
-          icon_url: '/src/assets/svg_icon/box.svg',
+          icon_url: 'src/assets/svg_icon/box.svg',
           text: '项目'
         }]
       }
