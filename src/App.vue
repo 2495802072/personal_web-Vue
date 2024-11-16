b920bd9462290a1b85a8770beffddc80b62189c8
顾霖轩 <2495802072@qq.com>
2024-11-16 19:52:51 +0800

    部署修缮

--- src/App.vue
+++ src/App.vue
@@ -12,13 +12,8 @@ let lookCardTimes = 0
 /* 方法 */
 function toggleCard(){ //控制nav 展开/not
   cardVisible.value = !cardVisible.value
-<<<<<<< Updated upstream
-  lookCardTimes += cardVisible.value
-=======
-  
   //实在不知道怎么充实card，随便加点功能，有灵感再改
   lookCardTimes += cardVisible.value ? 1:0
->>>>>>> Stashed changes
 }
 
 </script>
